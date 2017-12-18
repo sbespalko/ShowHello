3483544efd52479defe3179c3b92579911feaec6
bespalko.sergey <bespalko@i-teco.ru>
2017-12-18 17:13:14 +0300

    add random

--- src/main/java/ru/sbespalko/App.java
+++ src/main/java/ru/sbespalko/App.java
@@ -1,9 +1,14 @@
+package ru.sbespalko;
 /**
  * @author bespalko
  * @since 18.12.2017
  */
 public class App {
+  public void showNumber() {
+    System.out.printf("Random number: %2.2f", Math.random()*100);
+  }
   public static void main(String[] args) {
     System.out.println("Hello " + App.class);
+    new App().showNumber();
   }
 }
