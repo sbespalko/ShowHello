93c0e514624fc2e299c153848337053f5cf4b11e
bespalko.sergey <bespalko@i-teco.ru>
2017-12-18 17:45:00 +0300

    add gitsubmodule

--- src/main/java/ru/sbespalko/AppShower.java
+++ src/main/java/ru/sbespalko/AppShower.java
@@ -3,12 +3,12 @@ package ru.sbespalko;
  * @author bespalko
  * @since 18.12.2017
  */
-public class App {
+public class AppShower {
   public void showNumber() {
     System.out.printf("Random number: %2.2f", Math.random()*100);
   }
   public static void main(String[] args) {
-    System.out.println("Hello " + App.class);
-    new App().showNumber();
+    System.out.println("Hello " + AppShower.class);
+    new AppShower().showNumber();
   }
 }
