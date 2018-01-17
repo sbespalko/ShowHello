f3f7de217859d9bbd4dee8d6e996d4f85bdd31c6
bespalko.sergey <bespalko@i-teco.ru>
2018-01-17 11:50:45 +0300

    aop adding

--- src/main/java/ru/sbespalko/AppShower.java
+++ src/main/java/ru/sbespalko/AppShower.java
@@ -1,4 +1,7 @@
 package ru.sbespalko;
+
+import java.util.LinkedHashMap;
+
 /**
  * @author bespalko
  * @since 18.12.2017
@@ -12,5 +15,14 @@ public class AppShower {
     new AppShower().showNumber();
     System.out.println("exit from" + AppShower.class);
     System.out.println("dirty?");
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+    map.put("1", 1);
+    map.put("2", 2);
+    map.put("3", 3);
+    map.put("2", 10);
+    map.remove("2");
+    map.put("2", 20);
+    map.replace("3", 30);
+    System.out.println(map.toString());
   }
 }
